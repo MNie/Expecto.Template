0ffe2948f34a4d59c4b6aec2984553700255d83c
Michał Niegrzybowski <michal.niegrzybowski@gmail.com>
2018-05-08 17:42:31 +0200

    Update Expecto

--- createPackage.sh
+++ createPackage.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
+rm -r nuget
+mkdir nuget
+cd nuget
 mkdir content
-mv *.fs content/
-mv *.fsproj content/
-nuget pack Expecto.Template.nuspec
+cp ../*.fsproj content/
+cp ../*.fs content/
+cp -R ../.template.config content/
+cp ../Expecto.Template.nuspec .
+../../nuget.exe pack Expecto.Template.nuspec
